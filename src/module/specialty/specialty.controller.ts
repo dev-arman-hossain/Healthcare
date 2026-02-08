2b7b273021c9b3a209314b28ea540417b4454ffc
Arman Hossain <web.devarman@gmail.com>
2026-02-09 03:24:45 +0600

    reduce repitation code

--- src/module/specialty/specialty.controller.ts
+++ src/module/specialty/specialty.controller.ts
@@ -1,61 +1,39 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { SpecialtyService } from "./specialty.service";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
-const createSpecialty = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
+const createSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const specialty = await SpecialtyService.createSpecialty(req.body);
+  sendResponse(res, {
+    httpStatusCode: 201,
+    success: true,
+    message: "Specialty created successfully",
+    data: specialty,
+  });
+});
 
-    const specialty = await SpecialtyService.createSpecialty(payload);
-    res.status(201).json({
-      success: true,
-      message: "Specialty created successfully",
-      data: specialty,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: "Faild to create specialty",
-      error: err.message,
-    });
-  }
-};
+const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
+  const specialties = await SpecialtyService.getAllSpecialties();
+  sendResponse(res, {
+    httpStatusCode: 200,
+    success: true,
+    message: "Specialties fetched successfully",
+    data: specialties,
+  });
+});
 
-const getAllSpecialties = async (req: Request, res: Response) => {
-  try {
-    const specialties = await SpecialtyService.getAllSpecialties();
-    res.status(200).json({
-      success: true,
-      message: "Specialties fetched successfully",
-      data: specialties,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: "Faild to fetch specialties",
-      error: err.message,
-    });
-  }
-};
-
-const deleteSpecialty = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const deleteSpecialty = await SpecialtyService.deleteSpecialty(
-      id as string,
-    );
-    res.status(200).json({
-      success: true,
-      message: "Specialty deleted successfully",
-      data: deleteSpecialty,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: "Faild to delete specialty",
-      error: err.message,
-    });
-  }
-};
+const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const specialty = await SpecialtyService.deleteSpecialty(
+    req.params.id as string,
+  );
+  sendResponse(res, {
+    httpStatusCode: 200,
+    success: true,
+    message: "Specialty deleted successfully",
+    data: specialty,
+  });
+});
 
 export const SpecialtyController = {
   createSpecialty,
---
+++ src/shared/catchAsync.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (err: any) {
+      res.status(500).json({
+        success: false,
+        message: "Faild",
+        error: err.message,
+      });
+    }
+  };
+};
