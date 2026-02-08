87b00a71fed04191bbfb22423e7253de030f492a
Arman Hossain <web.devarman@gmail.com>
2026-02-09 04:48:22 +0600

    create auth module

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import { SpecialtyRoute } from "./module/specialty/specialty.route";
+import { authRoutes } from "./module/auth/auth.route";
 
 const app: Application = express();
 
 app.use(express.json());
 
+app.use("/api/v1/", authRoutes);
 app.use("/api/v1", SpecialtyRoute);
 
 app.get("/", (req: Request, res: Response) => {
