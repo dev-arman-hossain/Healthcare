7dfac9be5a3e4f16d267b0a9d6cfcf6d63c4151a
Arman Hossain <web.devarman@gmail.com>
2026-02-15 02:13:45 +0600

    Create get all doctors route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { UserRoutes } from "../module/user/user.route";
 import { SpecialtyRoute } from "../module/specialty/specialty.route";
 import { authRoutes } from "../module/auth/auth.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/specialties", SpecialtyRoute)
 router.use("/users", UserRoutes)
+router.use("/doctors", DoctorRoutes)
 
 
 export const IndexRoutes = router;
